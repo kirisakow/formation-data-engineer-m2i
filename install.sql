3045b3f3d2aa118a02fbcef73700e59fe7cb5f6d
Kiril Isakov <kirix.isakov@gmail.com>
2023-04-12 14:30:12 +0200

    add `install.sql`

---
+++ install.sql
@@ -0,0 +1,13 @@
+-- créations des bases de données
+CREATE DATABASE IF NOT EXISTS exercice;
+
+-- création des tables
+CREATE TABLE clients (
+    id INT PRIMARY KEY,
+    prenom VARCHAR(255),
+    nom VARCHAR(255),
+    email VARCHAR(255),
+    profession VARCHAR(255),
+    pays VARCHAR(255),
+    ville VARCHAR(255)
+);
