8af8b0c1ff88fb7070dcbb245c82792e99d31f58
Kiril Isakov <kirix.isakov@gmail.com>
2023-04-28 10:04:57 +0200

    encore 2 exercices

---
+++ 13_exercice_interface_test_user.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Créer un script qui vous propose le menu suivant :
+# 1 - Verifier l'existance d'un utilisateur
+# 2 - Connaitre l'UID d'un utilisateur
+# 3 - Quitter
+# L'utilisateur devra être saisi, à l'aide d'une fonction. Son existence devra être vérifiée à l'aide d'une autre fonction.
+# Rappel du format du fichier /etc/passwd qui contient les 7 champs suivants :
+# <User name>:<Encrypted password>:<User ID>:<User's group ID>:<User's full name>:<User home directory>:<Login shell>
+
---
+++ 14_exercice_ls_inverse.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Créer un script qui renvoie la même sortie que la commande ls mais dans l'ordre décroissant.
+# Vous ne devez ni vous servir de la commande ls, ni de la commande sort.
+
