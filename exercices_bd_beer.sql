a4cb2829c4ccac6b62ae77ca3c7645df5df7c766
Kiril Isakov <kirix.isakov@gmail.com>
2023-05-05 16:04:00 +0200

    format

--- exercices_bd_beer.sql
+++ exercices_bd_beer.sql
@@ -1,7 +1,7 @@
 # Exercices avec la base de données MySQL `beer`
 
 ### 1. Quels sont les tickets qui comportent l’article d’ID 500, afficher le numéro de ticket uniquement ?
-```sql
+
 SELECT DISTINCT
     ticket.NUMERO_TICKET
 FROM
@@ -36,9 +36,9 @@ WHERE
 -- 2626
 -- 3004
 -- 3479
-```
+
 ### 2. Afficher les tickets du 15/01/2014.
-```sql
+
 SELECT
     numero_ticket, date_vente
 FROM
@@ -48,9 +48,9 @@ WHERE
 ;
 -- NUMERO_TICKET, DATE_VENTE
 -- 37	2014-01-15 00:00:00
-```
+
 ### 3. Afficher les tickets émis du 15/01/2014 et le 17/01/2014.
-```sql
+
 SELECT
     NUMERO_TICKET, DATE_VENTE
 FROM
@@ -63,18 +63,18 @@ WHERE
 -- 38	2014-01-17 00:00:00
 -- 39	2014-01-17 00:00:00
 -- 40	2014-01-17 00:00:00
-```
+
 ### 4. Editer la liste des articles apparaissant à 50 et plus exemplaires sur un ticket.
-```sql
+
 select distinct article.ID_ARTICLE, NOM_ARTICLE from article
 join ventes on ventes.ID_ARTICLE = article.ID_ARTICLE
 where ventes.QUANTITE >= 50
 order by ID_ARTICLE
 ;
 -- 1090 row(s) returned
-```
+
 ### 5. Quelles sont les tickets émis au mois de mars 2014.
-```sql
+
 SELECT
     NUMERO_TICKET
 FROM
@@ -83,9 +83,9 @@ WHERE
     DATE_VENTE LIKE '2014-03%'
 ;
 -- 78 row(s) returned	0,000 sec / 0,000 sec
-```
+
 ### 6. Quelles sont les tickets émis entre les mois de mars et avril 2014 ?
-```sql
+
 SELECT
     NUMERO_TICKET, DATE_VENTE
 FROM
@@ -94,9 +94,9 @@ WHERE
     ticket.DATE_VENTE BETWEEN '2014-03-01' AND '2014-04-30'
 ;
 -- 166 row(s) returned	0,000 sec / 0,000 sec
-```
+
 ### 7. Quelles sont les tickets émis au mois de mars et juin 2014 ?
-```sql
+
 SELECT
     NUMERO_TICKET, DATE_VENTE
 FROM
@@ -106,9 +106,9 @@ WHERE
         AND YEAR(ticket.DATE_VENTE) = 2014
 ;
 -- 174 row(s) returned	0,000 sec / 0,000 sec
-```
+
 ### 8. Afficher la liste des bières classée par couleur. (Afficher l’id et le nom)
-```sql
+
 SELECT
     ID_ARTICLE, NOM_ARTICLE
 FROM
@@ -116,9 +116,9 @@ FROM
 ORDER BY id_couleur ASC , ID_ARTICLE ASC
 ;
 -- 3922 row(s) returned	0,000 sec / 0,000 sec
-```
+
 ### 9. Afficher la liste des bières n’ayant pas de couleur. (Afficher l’id et le nom)
-```sql
+
 SELECT
     ID_ARTICLE, NOM_ARTICLE
 FROM
@@ -128,9 +128,9 @@ WHERE
 ORDER BY id_couleur ASC , ID_ARTICLE ASC
 ;
 -- 706 rows
-```
+
 ### 10. Lister pour chaque ticket la quantité totale d’articles vendus. (Classer par quantité décroissante)
-```sql
+
 SELECT
     NUMERO_TICKET, SUM(QUANTITE)
 FROM
@@ -139,9 +139,9 @@ GROUP BY NUMERO_TICKET
 ORDER BY NUMERO_TICKET ASC
 ;
 -- 4502 rows
-```
+
 ### 11. Lister chaque ticket pour lequel la quantité totale d’articles vendus est supérieure à 500. (Classer par quantité décroissante)
-```sql
+
 SELECT
     NUMERO_TICKET, SUM(QUANTITE) AS qte_totale
 FROM
@@ -151,9 +151,9 @@ HAVING qte_totale > 500
 ORDER BY qte_totale DESC
 ;
 -- 1026 rows
-```
+
 ### 12. Lister chaque ticket pour lequel la quantité totale d’articles vendus est supérieure à 500. On exclura du total, les ventes ayant une quantité supérieure à 50 (classer par quantité décroissante)
-```sql
+
 SELECT
     NUMERO_TICKET, SUM(QUANTITE) AS qte_totale
 FROM
@@ -165,9 +165,9 @@ HAVING qte_totale > 500
 ORDER BY qte_totale DESC
 ;
 -- 901 rows
-```
+
 ### 13. Lister les bières de type ‘Trappiste’. (id, nom de la bière, volume et titrage)
-```sql
+
 SELECT
     ID_ARTICLE, NOM_ARTICLE, VOLUME, TITRAGE
 FROM
@@ -178,9 +178,9 @@ WHERE
     NOM_TYPE = 'Trappiste'
 ;
 -- 48 rows
-```
+
 ### 14. Lister les marques de bières du continent ‘Afrique’
-```sql
+
 SELECT
     ID_MARQUE, NOM_MARQUE
 FROM
@@ -193,9 +193,9 @@ WHERE
     NOM_CONTINENT = 'Afrique'
 ;
 -- 3 rows
-```
+
 ### 15. Lister les bières du continent ‘Afrique’
-```sql
+
 SELECT
     ID_ARTICLE, NOM_ARTICLE
 FROM
@@ -210,9 +210,9 @@ WHERE
     NOM_CONTINENT = 'Afrique'
 ;
 -- 6 rows (3 duplicates)
-```
+
 ### 16. Lister les tickets (année, numéro de ticket, montant total payé). En sachant que le prix de vente est égal au prix d’achat augmenté de 15% et que l’on n’est pas assujetti à la TVA.
-```sql
+
 SELECT
     ANNEE,
     NUMERO_TICKET,
@@ -234,9 +234,9 @@ JOIN article USING(ID_ARTICLE)
 GROUP BY ANNEE , NUMERO_TICKET
 ;
 -- 8263 rows in 0.016
-```
+
 ### 17. Donner le C.A. par année.
-```sql
+
 SELECT
     ANNEE,
     REPLACE(
@@ -255,9 +255,9 @@ GROUP BY ANNEE
 -- 2014   585 094.47
 -- 2015   1 513 663.13
 -- 2016   2 508 162.52
-```
+
 ### 18. Lister les quantités vendues de chaque article pour l’année 2016.
-```sql
+
 SELECT
     ID_ARTICLE,
     sum(QUANTITE) AS "qté vendue de chaque art"
@@ -267,9 +267,9 @@ WHERE ANNEE = 2016
 GROUP BY ANNEE, ID_ARTICLE
 ;
 -- 3922 rows
-```
+
 ### 19. Lister les quantités vendues de chaque article pour les années 2014,2015,2016.
-```sql
+
 SELECT
     ID_ARTICLE,
     sum(QUANTITE) AS "qté vendue de chaque art"
@@ -279,9 +279,9 @@ WHERE ANNEE IN (2014, 2015, 2016)
 GROUP BY ANNEE, ID_ARTICLE
 ;
 -- 11197 rows
-```
+
 ### 20. Lister les articles qui n’ont fait l’objet d’aucune vente en 2014.
-```sql
+
 SELECT ID_ARTICLE, NOM_ARTICLE
 FROM article
 WHERE ID_ARTICLE NOT IN (
@@ -307,9 +307,9 @@ LEFT JOIN (
 WHERE t1.ID_ARTICLE IS NULL
 ;
 -- 540 rows in 0.015
-```
+
 ### 21. Coder de 3 manières différentes la requête suivante : Lister les pays qui fabriquent des bières de type ‘Trappiste’.
-```sql
+
 SELECT DISTINCT NOM_PAYS
 FROM pays
 JOIN marque USING(ID_PAYS)
@@ -335,9 +335,9 @@ WHERE ID_PAYS in (
 )
 ;
 -- 3 rows
-```
+
 ### 22. Lister les tickets sur lesquels apparaissent un des articles apparaissant aussi sur le ticket 2014-856 (le ticket 856 de l’année 2014).
-```sql
+
 SELECT DISTINCT NUMERO_TICKET
 FROM ventes
 WHERE ID_ARTICLE IN (
@@ -348,9 +348,9 @@ WHERE ID_ARTICLE IN (
 )
 ;
 -- 37 rows
-```
+
 ### 23. Lister les articles ayant un degré d’alcool plus élevé que la plus forte des trappistes.
-```sql
+
 SELECT ID_ARTICLE
 FROM article
 WHERE titrage > (
@@ -361,9 +361,9 @@ WHERE titrage > (
 )
 ;
 -- 74 rows
-```
+
 ### 24. Éditer les quantités vendues pour chaque couleur en 2014.
-```sql
+
 SELECT
     ID_Couleur,
     NOM_COULEUR,
@@ -380,9 +380,9 @@ GROUP BY ID_Couleur, NOM_COULEUR
 -- 2           Blanche      14416
 -- 3           Blonde       72569
 -- 4           Brune        49842
-```
+
 ### 25. Donner pour chaque fabricant, le nombre de tickets sur lesquels apparait un de ses produits en 2014.
-```sql
+
 SELECT
 	ID_FABRICANT,
     COUNT(DISTINCT NUMERO_TICKET) as nb_tickets_2014_avec_articles_du_fabricant
@@ -419,9 +419,9 @@ GROUP BY ID_FABRICANT
 -- 8             35
 -- 9             27
 -- 10            13
-```
+
 ### 26. Donner l’ID, le nom, le volume et la quantité vendue des 20 articles les plus vendus en 2016.
-```sql
+
 SELECT ID_ARTICLE, NOM_ARTICLE, VOLUME, SUM(QUANTITE) as quantite_totale
 FROM article
 JOIN ventes USING(ID_ARTICLE)
@@ -451,9 +451,9 @@ LIMIT 20
 -- 2305         Hini Du                          75       491
 -- 1110         Mamouche                         33       489
 -- 3789         Methusalem                       75       488
-```
+
 ### 27. Donner l’ID, le nom, le volume et la quantité vendue des 5 ‘Trappistes’ les plus vendus en 2016.
-```sql
+
 SELECT ID_ARTICLE, NOM_ARTICLE, VOLUME, SUM(QUANTITE) as quantite_totale
 FROM article
 JOIN ventes USING(ID_ARTICLE)
@@ -470,9 +470,9 @@ LIMIT 5
 -- 120          Chimay Première   33       393
 -- 2101         Achel Brune       75       371
 -- 2104         Rochefort 6       75       357
-```
+
 ### 28. Donner l’ID, le nom, le volume et les quantités vendues en 2015 et 2016, des bières dont les ventes ont été stables. (Moins de 1% de variation)
-```sql
+
 SELECT
 	ID_ARTICLE, NOM_ARTICLE, VOLUME,
     t2015.quantite_vendue AS quantite_vendue_2015,
@@ -499,32 +499,31 @@ WHERE ABS((t2016.quantite_vendue - t2015.quantite_vendue) / t2015.quantite_vendu
 ORDER BY variation_2015_2016 asc
 ;
 -- 29 row(s) returned	0.156 sec
-```
+
 ### 29. Lister les types de bières suivant l’évolution de leurs ventes entre 2015 et 2016. Classer le résultat par ordre décroissant des performances.
-```sql
 
-```
+
+
 ### 30. Existe-t-il des tickets sans vente ?
-```sql
 
-```
+
+
 ### 31. Lister les produits vendus en 2016 dans des quantités jusqu’à -15% des quantités de l’article le plus vendu.
-```sql
 
-```
+
+
 ### 32. Appliquer une augmentation de tarif de 10% pour toutes les bières ‘Trappistes’ de couleur ‘Blonde’
-```sql
 
-```
+
+
 ### 33. Mettre à jour le degré d’alcool des toutes les bières n’ayant pas cette information. On y mettra le degré d’alcool de la moins forte des bières du même type et de même couleur.
-```sql
 
-```
+
+
 ### 34. Suppression des bières qui ne sont pas des bières ! (Type ‘Bière Aromatisée’)
-```sql
 
-```
+
+
 ### 35. Supprimer les tickets qui n’ont pas de ventes.
-```sql
 
-```
+
