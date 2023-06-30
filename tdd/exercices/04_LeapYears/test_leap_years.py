46a98bcae24456681bcdd1fc3e233f756d86ac29
Kiril Isakov <kirix.isakov@gmail.com>
2023-06-30 15:01:49 +0200

    exo Leap Years

--- tdd/exercices/04_LeapYears/test_leap_years.py
+++ tdd/exercices/04_LeapYears/test_leap_years.py
@@ -1,10 +1,37 @@
 import pytest
 import unittest
+from leap_years import is_leap_year
+
 
 def test_leap_years():
-    ...
+    assert is_leap_year(2008) == True
+
+
+def test_leap_years_2():
+    assert is_leap_year(1900) == False
+
+
+def test_leap_years_3():
+    assert is_leap_year(2400) == True
+
+
+def test_leap_years_4():
+    assert is_leap_year(1200) == False
 
 
 class TestLeapYears(unittest.TestCase):
     def test_leap_years(self):
-        ...
+        self.assertEqual(is_leap_year(2008), True)
+
+    def test_leap_years_2(self):
+        self.assertEqual(is_leap_year(1900), False)
+
+    def test_leap_years_3(self):
+        self.assertEqual(is_leap_year(2400), True)
+
+    def test_leap_years_4(self):
+        self.assertEqual(is_leap_year(1200), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
