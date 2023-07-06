9c999e0b505180530629bc82ec7a3a110de6fb8f
Kiril Isakov <kirix.isakov@gmail.com>
2023-07-06 17:13:18 +0200

    standalone CRUD app, tests OK with Postman

--- main.py
+++ main.py
@@ -1,60 +1,71 @@
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+app = Flask('my_flask_app')
 
-# Temporary storage for users
 users = [
-    {'id': 1, 'name': 'John Doe le papy', 'email': 'XXXXXX@XXXXX'},
-    {'id': 2, 'name': 'John Doe le daron', 'email': 'XXXXXX@XXXXX'},
-    {'id': 3, 'name': 'John Doe le jeune', 'email': 'XXXXXX@XXXXX'},
-    {'id': 4, 'name': 'Jane Doe la mamie', 'email': 'XXXXXX@XXXXX'},
-    {'id': 5, 'name': 'Jane Doe la daronne', 'email': 'XXXXXX@XXXXX'}
+    {'id': 1, 'name': 'John Doe le papy', 'email': 'papy@doe.me'},
+    {'id': 2, 'name': 'John Doe le daron', 'email': 'daron@doe.me'},
+    {'id': 3, 'name': 'John Doe le jeune', 'email': 'jeune@doe.me'},
+    {'id': 4, 'name': 'Jane Doe la mamie', 'email': 'mamie@doe.me'},
+    {'id': 5, 'name': 'Jane Doe la daronne', 'email': 'daronne@doe.me'}
 ]
 
-# CREATE: Create a user
-@app.route('/user', methods=['POST'])
+
+@app.route('/create_user', methods=['POST'])
+@app.route('/create_user/', methods=['POST'])
 def create_user():
     data = request.get_json()
+    if data is None:
+        return jsonify({'message': 'Cannot create a user: no data'}), 404
     user = {
         'id': len(users) + 1,
         'name': data['name'],
         'email': data['email']
     }
     users.append(user)
-    return jsonify(user), 201
+    return jsonify(users), 201
 
-# RETRIEVE: Get all users
+
 @app.route('/', methods=['GET'])
 @app.route('/users', methods=['GET'])
+@app.route('/users/', methods=['GET'])
+@app.route('/user', methods=['GET'])
+@app.route('/user/', methods=['GET'])
 def get_users():
     return jsonify(users)
 
-# RETRIEVE: Get a user by ID
-@app.route('/user/:user_id', methods=['GET'])
-def get_user(user_id):
-    user = next((user for user in users if user['id'] == user_id), None)
-    if user:
-        return jsonify(user)
-    return jsonify({'message': 'User not found'}), 404
 
-# UPDATE: Update a user
-@app.route('/user/:user_id', methods=['PUT'])
-def update_user(user_id):
+@app.route('/user/<int:user_id>', methods=['GET'])
+@app.route('/user/<int:user_id>/', methods=['GET'])
+def get_user(user_id: int):
     user = next((user for user in users if user['id'] == user_id), None)
-    if user:
-        data = request.get_json()
-        user['name'] = data['name']
-        user['email'] = data['email']
-        return jsonify(user)
-    return jsonify({'message': 'User not found'}), 404
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user)
 
-# DELETE: Delete a user
-@app.route('/user/:user_id', methods=['DELETE'])
-def delete_user(user_id):
+
+@app.route('/update_user/<int:user_id>', methods=['PUT'])
+@app.route('/update_user/<int:user_id>/', methods=['PUT'])
+def update_user(user_id: int):
+    for i, user in enumerate(users):
+        if user['id'] == user_id:
+            data = request.get_json()
+            if data is None:
+                return jsonify({'message': 'Cannot update a user: no data'}), 404
+            user['name'] = data['name']
+            user['email'] = data['email']
+            users.pop(i)
+            users.insert(i, user)
+            return jsonify(users)
+
+
+@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
+@app.route('/delete_user/<int:user_id>/', methods=['DELETE'])
+def delete_user(user_id: int):
     global users
     users = [user for user in users if user['id'] != user_id]
-    return jsonify({'message': 'User deleted'})
+    return jsonify(users)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9001, debug=True)
-
+    app.run(host='0.0.0.0', debug=True)
