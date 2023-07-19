3b75237847b1dfb5d7d94d52b0c0c0faf64b01dd
Kiril Isakov <kirix.isakov@gmail.com>
2023-07-19 11:39:37 +0200

    remove unrelated files

--- dags/file/mon_script.sh
+++
@@ -1,4 +0,0 @@
-if [[ ! -e /opt/airflow/dags/file/rand_number.txt ]]; then
-    mkdir -p "$AIRFLOW_HOME/dags/file"
-    touch "$AIRFLOW_HOME/dags/file/rand_number.txt"
-fi
--- dags/sql/create_drivers_table.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS drivers_data;
-CREATE TABLE drivers_data (
-    "school_year" TEXT,
-    "vendor_name" TEXT,
-    "type_of_service" TEXT,
-    "active_employees" TEXT,
-    "job_type" TEXT
-);
